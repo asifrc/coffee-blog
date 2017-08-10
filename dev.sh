8785fd3997d8d827fa7984ab2bb02f0f9f8f9ffd
Asif R Choudhury <choudhury.asif@gmail.com>
2017-08-09 21:10:22 -0500

    Replace building local dockerfile with dev script that pull from docker hub

---
+++ dev.sh
@@ -0,0 +1,1 @@
+docker run -it -p 3000:3000 -v $(pwd):/app -p 8000:8000 asifrc/gatsbyjs-dev:latest /bin/bash
--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -it -p 3000:3000 -v $(pwd):/app -w /app -p 8000:8000 gatsby:local /bin/bash
