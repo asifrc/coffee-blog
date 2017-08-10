55aa8c41d89aecd31b3789d002ac9d55825972ea
Asif R Choudhury <choudhury.asif@gmail.com>
2017-08-09 23:03:13 -0500

    Inject Typography styles in all environments

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,9 +3,7 @@ import Wordpress2016 from 'typography-theme-wordpress-2016'
 
 const typography = new Typography(Wordpress2016)
 
-// Hot reload typography in development.
-if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles()
-}
+// Hot reload typography everywhere.
+typography.injectStyles()
 
 export default typography
